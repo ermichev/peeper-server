a626451fe0642b974bf9b4bae0fd7980275688b2
Ermichev Aleksandr <a.ermich@gmail.com>
2026-02-04 23:02:42 +0900

    Add package base from Hummingbird template

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PeeperServer",
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18)],
+    products: [
+        .executable(name: "App", targets: ["App"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0", traits: [.defaults, "CommandLineArguments"]),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0")
+    ],
+    targets: [
+        .executableTarget(name: "App",
+            dependencies: [
+                .product(name: "Configuration", package: "swift-configuration"),
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "Hummingbird", package: "hummingbird"),
+            ],
+            path: "Sources/App"
+        ),
+        .testTarget(name: "AppTests",
+            dependencies: [
+                .byName(name: "App"),
+                .product(name: "HummingbirdTesting", package: "hummingbird")
+            ],
+            path: "Tests/AppTests"
+        )
+    ]
+)
