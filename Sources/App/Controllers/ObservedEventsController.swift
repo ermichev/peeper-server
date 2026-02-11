c55e619aaba7e530e8302d87efa3c0b0bc35442c
Ermichev Aleksandr <a.ermich@gmail.com>
2026-02-11 17:01:45 +0900

    Handle uploadScreenCapture and getScreenCapture endpoints

--- Sources/App/Controllers/ObservedEventsController.swift
+++ Sources/App/Controllers/ObservedEventsController.swift
@@ -6,22 +6,30 @@
 //
 
 import Dependencies
+import Foundation
 import Hummingbird
+import MultipartKit
 
 struct ObservedEventsController {
     
     @Dependency(\.eventRepository.notifyEvent) var handleEvent
+    @Dependency(\.eventRepository.uploadImage) var storeImage
     
     var endpoints: RouteCollection<AppRequestContext> {
         RouteCollection(context: AppRequestContext.self)
             .post("notify", use: notifyEvent)
+            .post("uploadScreenCapture", use: uploadImage)
     }
     
     struct NotifyEventRequest: Codable {
         let event: String
     }
     
-    func notifyEvent(request: Request, context: some RequestContext) async throws -> HTTPResponse.Status {
+    struct UploadImageRequest: Codable {
+        let image: Data
+    }
+    
+    private func notifyEvent(request: Request, context: some RequestContext) async throws -> HTTPResponse.Status {
         let request = try await request.decode(as: NotifyEventRequest.self, context: context)
         if try await handleEvent(request.event) {
             return .ok
@@ -29,5 +37,26 @@ struct ObservedEventsController {
             return .badRequest
         }
     }
+    
+    private func uploadImage(request: Request, context: some RequestContext) async throws -> HTTPResponse.Status {
+        let imageData = try await decodeMultipart(request: request)
+        guard try await storeImage(imageData) else { throw Errors.savingFailed }
+        return .ok
+    }
+    
+    private func decodeMultipart(request: Request) async throws -> Data {
+        let buffer = try await request.body.collect(upTo: 100_000_000)
+        guard let contentType = request.headers[.contentType] else { throw Errors.invalidRequest }
+        guard let boundary = contentType.firstMatch(of: /boundary=(.+)/)?.output.1 else { throw Errors.invalidRequest }
+        let typedRequest = try FormDataDecoder().decode(UploadImageRequest.self, from: buffer, boundary: String(boundary))
+        return typedRequest.image
+    }
+    
+    // -
+    
+    private enum Errors: Error {
+        case emptyBody
+        case invalidRequest
+        case savingFailed
+    }
 }
-
--- Sources/App/Controllers/ObserverRequestsController.swift
+++ Sources/App/Controllers/ObserverRequestsController.swift
@@ -12,14 +12,33 @@ import Hummingbird
 struct ObserverRequestsController {
     
     @Dependency(\.eventRepository.events) var fetchEvents
+    @Dependency(\.eventRepository.event) var findEvent
+    
+    @Dependency(\.imageRepository.isItemExist) var isImageExist
+    @Dependency(\.imageRepository.getItem) var fetchImage
     
     var endpoints: RouteCollection<AppRequestContext> {
         RouteCollection(context: AppRequestContext.self)
-            .get("events", use: events)
+            .get("getEvents", use: events)
+            .get("getScreenCapture", use: screenCapture)
     }
     
     func events(request: Request, context: some RequestContext) async throws -> [Event] {
         let from = TimeInterval(request.uri.queryParameters["from"] ?? "")
         return try await fetchEvents(from)
+    }
+    
+    func screenCapture(request: Request, context: some RequestContext) async throws -> ByteBuffer {
+        guard let idParam = request.uri.queryParameters["id"] else { throw Errors.missingArgument }
+        guard let imageId = UUID(uuidString: String(idParam)) else { throw Errors.incorrectArgument }
+        guard try await isImageExist(imageId) else { throw Errors.notFound }
+        let imageData = try await fetchImage(imageId)
+        return ByteBuffer(data: imageData)
+    }
+    
+    private enum Errors: Error {
+        case missingArgument
+        case incorrectArgument
+        case notFound
     }
 }
